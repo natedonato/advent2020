8288492a8242475acc391ee3e53f3c8334f22423
Nate Donato <natedonato@gmail.com>
2020-12-08 10:51:00 -0800

    day8

---
+++ day8/node/problem1.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const input = fs.readFileSync("day8/node/input.txt", "utf8").split("\n");
+
+class Computer{
+  constructor(input){
+    this.accumulator = 0;
+    this.currentInstruction = 0;
+  }
+
+  runComputer(){
+    let currentLine = input[this.currentInstruction];
+    if(this.executeLine(currentLine) === true){
+      this.runComputer();
+    }
+  }
+
+  executeLine(line){
+    let [operation, argument] = line.split(' ');
+    input[this.currentInstruction] = 'X +0';
+
+    switch(operation){
+      case 'jmp':
+        this.currentInstruction += parseInt(argument);
+        break;  
+      case 'X':
+        console.log('Final accumulator',this.accumulator);
+        return false;
+      case 'acc':
+        this.accumulator += parseInt(argument);
+      case 'nop':
+      default:
+        this.currentInstruction += 1;
+    }
+    return true;
+  }
+}
+
+const comp = new Computer(input);
+comp.runComputer();
---
+++ day8/node/problem2.js
@@ -0,0 +1,65 @@
+const fs = require("fs");
+const originalInput = fs
+  .readFileSync("day8/node/input.txt", "utf8")
+  .split("\n");
+
+class Computer {
+  constructor(input) {
+    this.input = input;
+    this.accumulator = 0;
+    this.currentInstruction = 0;
+    this.lastInstruction = input.length;
+  }
+
+  runComputer() {
+    let currentLine = this.input[this.currentInstruction];
+
+    if (this.currentInstruction === this.lastInstruction) {
+      console.log("Final accumulator", this.accumulator);
+      return true;
+    } else if (this.executeLine(currentLine) === true) {
+      this.runComputer();
+    } else {
+      return false;
+    }
+  }
+
+  executeLine(line) {
+    let [operation, argument] = line.split(" ");
+    this.input[this.currentInstruction] = "X +0";
+
+    switch (operation) {
+      case "jmp":
+        this.currentInstruction += parseInt(argument);
+        break;
+      case "X":
+        return false;
+      case "acc":
+        this.accumulator += parseInt(argument);
+      case "nop":
+      default:
+        this.currentInstruction += 1;
+    }
+    return true;
+  }
+}
+
+for (let i = 0; i < originalInput.length; i++) {
+  const inputCopy = [...originalInput];
+
+  let line = inputCopy[i];
+  let [operation, argument] = line.split(" ");
+
+  if (operation !== "nop" && operation !== "jmp") {
+    continue;
+  } else if (operation === "nop") {
+    inputCopy[i] = "jmp " + argument;
+  } else {
+    inputCopy[i] = "nop" + argument;
+  }
+
+  const comp = new Computer(inputCopy);
+  if (comp.runComputer() === true) {
+    break;
+  }
+}
