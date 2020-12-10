f3c0cee30c88c98396c03465c3b685a34ddf2791
natedonato <natedonato@gmail.com>
2020-12-10 11:10:08 -0800

    day 10

--- day10/node/problem1.js
+++ day10/node/problem1.js
@@ -1,15 +1,18 @@
 const fs = require("fs");
-let input = fs.readFileSync("day10/node/input.txt", "utf8").split("\r\n").map(el => parseInt(el)).sort((a,b) => a - b);
+let input = fs
+  .readFileSync("day10/node/input.txt", "utf8")
+  .split("\r\n")
+  .map((el) => parseInt(el))
+  .sort((a, b) => a - b);
 
-input.push(input[input.length -1] + 3)
+input.push(input[input.length - 1] + 3);
 input.unshift(0);
 
-let difs = {1: 0, 2:0, 3:0 };
+let difs = { 1: 0, 2: 0, 3: 0 };
 
-for(let i = 0; i < input.length - 1; i++){
-    const dif = input[i+1] - input[i];
-
-    difs[dif] += 1;
+for (let i = 0; i < input.length - 1; i++) {
+  const dif = input[i + 1] - input[i];
+  difs[dif] += 1;
 }
 
 console.log(difs);
